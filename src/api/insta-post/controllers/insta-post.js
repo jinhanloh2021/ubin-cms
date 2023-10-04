e5cecb8375fffd6910cb1305b8bc4f6074266869
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2023-10-04 21:00:08 +0800

    feat:
    Add schema for insta posts. Contain post URL, image URL, datetime.

---
+++ src/api/insta-post/controllers/insta-post.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * insta-post controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::insta-post.insta-post');
---
+++ src/api/insta-post/services/insta-post.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * insta-post service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::insta-post.insta-post');
