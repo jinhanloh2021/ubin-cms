c9c1981e756445a852c01c8fb1c93eefbf170d83
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2023-10-16 23:23:18 +0800

    Feat: Add InstaCover schema

---
+++ src/api/insta-media/controllers/insta-media.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * insta-media controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::insta-media.insta-media');
---
+++ src/api/insta-media/routes/insta-media.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * insta-media router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::insta-media.insta-media');
